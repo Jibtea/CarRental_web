6ca68c708e1bd705a9a6c66e514d9efc31e78347
Prueksachart Prueksakorn <prueksachart254949@gmail.com>
2025-03-22 23:21:20 +0700

    workshop12

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,44 @@
-import {configureStore} from "@reduxjs/toolkit";
+import {configureStore, combineReducers} from "@reduxjs/toolkit";
 import cartSlice from "./features/cartSlice";
 import { UseSelector, TypedUseSelectorHook, useSelector } from "react-redux";
+import {persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER} from "redux-persist";
+import createWebStorage from "redux-persist/lib/storage/createWebStorage";
+import { WebStorage } from "redux-persist/lib/types";
+
+function createPersistStorage(): WebStorage {
+    const isServer = typeof window === 'undefined';
+    //Return noop storage.
+    if(isServer){
+        return {
+            getItem(){
+                return Promise.resolve(null);
+            },
+            setItem(){
+                return Promise.resolve();
+            },
+            removeItem(){
+                return Promise.resolve();
+            },
+        };
+    }
+    return createWebStorage('local');
+}
+const storage = createPersistStorage()
+
+const persistConfig = {
+    key: "rootPersist",
+    storage
+}
+const rootReducer = combineReducers({cartSlice})
+const reduxPersistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-    reducer:{
-        cartSlice
-    }
+    reducer:reduxPersistedReducer,
+    middleware: (getDefaultMiddleware)=> getDefaultMiddleware({
+        serializableCheck: {
+            ignoredActions: [ FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+    })
 })
 
 export type RootState = ReturnType<typeof store.getState>
